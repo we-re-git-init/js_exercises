617710c6d8c283253482877b11b9291dcca5754d
Brian <riceb53@gmail.com>
2019-07-23 19:42:36 -0500

    add js practice

---
+++ loops.js
@@ -0,0 +1,24 @@
+// while loop
+
+
+// print numbers from 1-100
+
+var i = 0;
+
+while (i < 100) {
+  console.log(i);
+  i += 1;
+}
+
+
+var fruits = ['blackberry', 'raspberry', 'peach', 'cherry'];
+
+fruits.forEach(function(fruit) {
+  console.log(fruit);
+})
+
+
+
+for (var i = 0; i < 100; i++) {
+  console.log(i);
+}
